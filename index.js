8ecc205ac658fb6528355f2add1cbd9cca344e96
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2020-06-19 14:16:56 -0500

    chore: init

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const { spawnSync } = require('child_process')
+
+spawnSync('npm', 'add -D @babel/core @babel/register @babel/node @babel/preset-env @babel/plugin-transform-runtime'.split(' '), {
+  stdio: 'inherit'
+})
+
+spawnSync('npm', 'npm add @babel/runtime'.split(' '), {
+  stdio: 'inherit'
+})
+
+fs.writeFileSync('babel.config.json', `{
+  "presets": ["@babel/preset-env"],
+  "plugins": ["@babel/plugin-transform-runtime"]
+}`)
